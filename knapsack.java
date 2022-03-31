7d5761d5afb8a8e5cfdaa40e90499839c383011f
Braden Carlson <bradenjcarlson@live.com>
2022-03-30 20:28:20 -0600

    started decrypt method

--- knapsack.java
+++ knapsack.java
@@ -7,6 +7,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import java.lang.IllegalArgumentException;
+
 public class knapsack {
 
 	/** main method to test and utilize all methods defined in this
@@ -143,6 +145,73 @@ public class knapsack {
 				new BigInteger(Integer.toString(x8))));
 		}
 		return encryptedMessage;
+	}
+
+	/** Takes a sigle block of an encrypted message, and decrypts it,
+	*   by using the private sequence that is provided, to solve the
+	*   Knapsack problem of finding the original bits of the message.
+	*   This method retuns a byte array that will contain the original
+	*   message.
+	*   @param message the BigInteger corresponding to the encrypted block
+	*   @param privateSequence The private sequence
+	*   @return a byte array containing the original message
+	*/
+	public static Byte[] decrypt(BigInteger message, BigInteger[] privateSequence) {
+		int one = 0x01;	// first bit
+		int two = 0x02; // second bit
+		int four = 0x04; // ...
+		int eight = 0x08;
+		int sixteen = 0x10;
+		int thirtytwo = 0x20;
+		int sixtyfour = 0x40;
+		int onetwentyeight = 0x80; // eighth bit.
+
+		int position = privateSequence.length-1;
+		boolean[] bits = new boolean[8];
+
+		byte[] decrypted = new byte[privateSequence/8];
+		int count = 0;
+
+		while (message.compareTo(BigInteger.ZERO)>0) {
+			for(int i = 0; i< 8; i++) {
+				if (message.compareTo(privateSequence[position--])>0) {
+					bits[i] = true;
+				} else {
+					bits[0] = false;
+				}
+			}
+			byte myByte = toByte(bits);
+			decrypted[count++] = myByte;
+		}
+
+		return decrypted;
+	}
+
+	public static byte toByte(boolean[] array) throws IllegalArgumentException {
+		if (array.length==8) {
+			int[] bits = new int[8];
+
+			bits[0] =	0b00000001;
+			bits[1] = 0b00000010;
+			bits[2] = 0b00000100;
+			bits[3] = 0b00001000;
+			bits[4] = 0b00010000;
+			bits[5] = 0b00100000;
+			bits[6] =	0b01000000;
+			bits[7] = 0b10000000;
+
+			byte newByte = 0x00;
+
+			for(int i = 0; i< 8; i++) {
+				if(array[i]) {
+					newByte = newByte & bits[i];
+				}
+			}
+			return newByte;
+		}
+		else {
+			throw new IllegalArgumentException("To create a new byte, an array length\nof 8 is required.");
+		}
 	}
 
 	/** Takes a private sequence and uses the given multiplier and modulus
